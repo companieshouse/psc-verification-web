7d43c7e39e3bfb0f53987213a5447c454d77bef5
chrislewis <clewis1@companieshouse.gov.uk>
2023-12-07 14:37:40 +0000

    Removed node starter code and updated index page with sample code.

--- src/router.dispatch.ts
+++ src/router.dispatch.ts
@@ -1,14 +1,15 @@
 // Do Router dispatch here, i.e. map incoming routes to appropriate router
-import { Application, Request, Response } from "express";
+import { Application, Request, Response, Router } from "express";
 import indexRouter from "./routers/indexRouter";
-import userRouter from "./routers/userRouter";
-import companyRouter from "./routers/companyRouter";
 
 const routerDispatch = (app: Application) => {
-    app.use("/", indexRouter);
-    app.use("/user", userRouter);
-    app.use("/company", companyRouter);
-    app.use("*", (req: Request, res: Response) => {
+
+    const router = Router();
+    // Required for endpoint prefix
+    app.use("/persons-with-significant-control-verification", router);
+
+    router.use("/", indexRouter);
+    router.use("*", (req: Request, res: Response) => {
         res.status(404).render("partials/error_400");
     });
 };
