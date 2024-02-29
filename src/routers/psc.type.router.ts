e5daddbdab1781c9bf844af0dc87a99867b35593
eedwards0 <eedwards2@companieshouse.gov.uk>
2024-02-29 10:46:30 +0000

    IDVA3-565
     WIP

---
+++ src/routers/psc.type.router.ts
@@ -0,0 +1,17 @@
+import { NextFunction, Request, Response, Router } from "express";
+import { PscTypeHandler } from "./handlers/psc_type/psc_type";
+import { handleExceptions } from "../utils/async.handler";
+import logger from "../lib/Logger";
+
+const router: Router = Router();
+
+router.get("/", handleExceptions(async (req: Request, res: Response, _next: NextFunction) => {
+    const handler = new PscTypeHandler();
+    const params = handler.execute(req, res);
+
+    if (params.templatePath && params.viewData) {
+        res.render(params.templatePath, params.viewData);
+    }
+}));
+
+export default router;
