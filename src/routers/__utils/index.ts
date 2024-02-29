8f1c4a30efeff3ed95cce6d56bdf163974ca24bf
PhilipGlover <pglover@companieshouse.gov.uk>
2024-02-29 10:51:05 +0000

    converting skeleton one page to confirm company

--- src/routers/__utils/index.ts
+++ src/routers/__utils/index.ts
@@ -1,8 +1,8 @@
 import StartRouter from "./../startRouter";
-import SkeletonOneRouter from "./../skeleton.one.router";
+import ConfirmCompanyRouter from "./../confirmCompanyRouter";
 import SkeletonTwoRouter from "./../skeleton.two.router";
 import SkeletonThreeRouter from "./../skeleton.three.router";
 import SkeletonFourRouter from "./../skeleton.four.router";
 import SkeletonFiveRouter from "./../skeleton.five.router";
 import SkeletonSixRouter from "./../skeleton.six.router";
-export { StartRouter, SkeletonOneRouter, SkeletonTwoRouter, SkeletonThreeRouter, SkeletonFourRouter, SkeletonFiveRouter, SkeletonSixRouter };
+export { StartRouter, ConfirmCompanyRouter, SkeletonTwoRouter, SkeletonThreeRouter, SkeletonFourRouter, SkeletonFiveRouter, SkeletonSixRouter };
