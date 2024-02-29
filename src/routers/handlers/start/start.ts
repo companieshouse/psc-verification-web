eee6a8c8e840b6c8c8b42ac7af114a4a29167f56
Alison Griffiths <agriffiths1@companieshouse.gov.uk>
2024-02-29 09:12:51 +0000

    Refactored the locale functionality to be handled by the handler

--- src/routers/handlers/start/start.ts
+++ src/routers/handlers/start/start.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from "express";
 import { BaseViewData, GenericHandler, ViewModel } from "./../generic";
 import logger from "../../../lib/Logger";
+import { PrefixedUrls } from "../../../constants";
+import { LocalesService } from "@companieshouse/ch-node-utils";
+import { selectLang, getLocalesService, getLocaleInfo } from "../../../utils/localise";
 
 export class StartHandler extends GenericHandler<BaseViewData> {
 
@@ -8,10 +11,15 @@ export class StartHandler extends GenericHandler<BaseViewData> {
 
     public getViewData (req: Request): BaseViewData {
         const baseViewData = super.getViewData(req);
+        // adding language functionality
+        const lang = selectLang(req.query.lang);
+        const locales = getLocalesService();
 
         return {
             ...baseViewData,
+            ...getLocaleInfo(locales, lang),
             title: "PSC Verification",
+            currentUrl: PrefixedUrls.START,
             backURL: null
         };
     }
@@ -19,6 +27,7 @@ export class StartHandler extends GenericHandler<BaseViewData> {
     public execute (req: Request, _response: Response): ViewModel<BaseViewData> {
         logger.info(`GET request to serve start page`);
         // ...process request here and return data for the view
+
         return {
             templatePath: StartHandler.templatePath,
             viewData: this.getViewData(req)
