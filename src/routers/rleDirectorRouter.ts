d02a68d8a550d4d657fc22c825230bed5d14ef15
JasSmith-ch <jsmith3@companieshouse.gov.uk>
2024-04-08 14:28:29 +0100

    align files with main branch

--- src/routers/rleDirectorRouter.ts
+++ src/routers/rleDirectorRouter.ts
@@ -1,7 +1,6 @@
-import { NextFunction, Request, Response, Router } from "express";
-import { RleDirectorHandler } from "./handlers/rleDirector/rleDirector";
-import { handleExceptions } from "../utils/async.handler";
-import { logger } from "../lib/Logger";
+import { Request, Response, Router } from "express";
+import { RleDirectorHandler } from "./handlers/rle-director/rleDirector";
+import { handleExceptions } from "../utils/asyncHandler";
 const router: Router = Router();
 
 router.get("/", handleExceptions(async (req: Request, res: Response) => {
