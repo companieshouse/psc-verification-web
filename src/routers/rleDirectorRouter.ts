f16c8511206d25dc740fc52affd6ac9d45bb6ad0
JasSmith-ch <jsmith3@companieshouse.gov.uk>
2024-04-08 11:48:02 +0100

    taken the dynamic content out of the json file and into nunjucks

--- src/routers/rleDirectorRouter.ts
+++ src/routers/rleDirectorRouter.ts
@@ -1,7 +1,7 @@
 import { NextFunction, Request, Response, Router } from "express";
 import { RleDirectorHandler } from "./handlers/rleDirector/rleDirector";
 import { handleExceptions } from "../utils/async.handler";
-import logger from "../lib/Logger";
+import { logger } from "../lib/Logger";
 const router: Router = Router();
 
 router.get("/", handleExceptions(async (req: Request, res: Response) => {
