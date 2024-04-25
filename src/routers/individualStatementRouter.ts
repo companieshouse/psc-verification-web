e1d2090c50499d27b7f58097d06671c6c4b7aa75
PhilipGlover <pglover@companieshouse.gov.uk>
2024-04-25 11:40:49 +0100

    add remaining individual journey screens to use forms and POST requests

--- src/routers/individualStatementRouter.ts
+++ src/routers/individualStatementRouter.ts
@@ -1,7 +1,10 @@
-import { Request, Response, Router } from "express";
-import { Urls } from "../constants";
+import { NextFunction, Request, Response, Router } from "express";
+import { PrefixedUrls, Urls } from "../constants";
 import { authenticate } from "../middleware/authentication";
 import { handleExceptions } from "../utils/asyncHandler";
+import { selectLang } from "../utils/localise";
+import { addSearchParams } from "../utils/queryParams";
+import { getUrlWithTransactionIdAndSubmissionId } from "../utils/url";
 import { IndividualStatementHandler } from "./handlers/individual-statement/individualStatement";
 
 const router: Router = Router();
@@ -10,6 +13,13 @@ router.get(Urls.INDIVIDUAL_STATEMENT, authenticate, handleExceptions(async (req:
     const handler = new IndividualStatementHandler();
     const { templatePath, viewData } = await handler.executeGet(req, res);
     res.render(templatePath, viewData);
+}));
+
+router.post(Urls.INDIVIDUAL_STATEMENT, authenticate, handleExceptions(async (req: Request, res: Response, _next: NextFunction) => {
+    const lang = selectLang(req.body.lang);
+
+    const nextPageUrl = getUrlWithTransactionIdAndSubmissionId(PrefixedUrls.PSC_VERIFIED, req.params.transactionId, req.params.submissionId);
+    res.redirect(addSearchParams(nextPageUrl, { lang }));
 }));
 
 export default router;
--- src/routers/personalCodeRouter.ts
+++ src/routers/personalCodeRouter.ts
@@ -1,8 +1,11 @@
-import { Request, Response, Router } from "express";
-import { Urls } from "../constants";
+import { NextFunction, Request, Response, Router } from "express";
+import { PrefixedUrls, Urls } from "../constants";
 import { authenticate } from "../middleware/authentication";
 import { handleExceptions } from "../utils/asyncHandler";
 import { PersonalCodeHandler } from "./handlers/personal-code/personalCode";
+import { selectLang } from "../utils/localise";
+import { getUrlWithTransactionIdAndSubmissionId } from "../utils/url";
+import { addSearchParams } from "../utils/queryParams";
 
 const router: Router = Router();
 
@@ -10,6 +13,13 @@ router.get(Urls.PERSONAL_CODE, authenticate, handleExceptions(async (req: Reques
     const handler = new PersonalCodeHandler();
     const { templatePath, viewData } = await handler.executeGet(req, res);
     res.render(templatePath, viewData);
+}));
+
+router.post(Urls.PERSONAL_CODE, authenticate, handleExceptions(async (req: Request, res: Response, _next: NextFunction) => {
+    const lang = selectLang(req.body.lang);
+
+    const nextPageUrl = getUrlWithTransactionIdAndSubmissionId(PrefixedUrls.INDIVIDUAL_STATEMENT, req.params.transactionId, req.params.submissionId);
+    res.redirect(addSearchParams(nextPageUrl, { lang }));
 }));
 
 export default router;
