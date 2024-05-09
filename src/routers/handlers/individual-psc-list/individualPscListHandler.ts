1d0ea721a55934c8d25ac822680888ad20b52ae5
eedwards0 <eedwards2@companieshouse.gov.uk>
2024-05-09 08:50:27 +0100

    Add dynamic content to web page WIP

--- src/routers/handlers/individual-psc-list/individualPscListHandler.ts
+++ src/routers/handlers/individual-psc-list/individualPscListHandler.ts
@@ -4,18 +4,21 @@ import { logger } from "../../../lib/logger";
 import { getLocaleInfo, getLocalesService, selectLang } from "../../../utils/localise";
 import { getUrlWithTransactionIdAndSubmissionId } from "../../../utils/url";
 import { BaseViewData, GenericHandler, ViewModel } from "../generic";
-// import { CompanyPersonWithSignificantControlResource } from "@companieshouse/api-sdk-node/dist/services/company-psc/types";
-// import { getCompanyIndividualPscList } from "services/companyPscService";
-
-interface PscIndividualListViewData extends BaseViewData {
-    companyName: String
-    pscIndividuals: Map <String, Date>
+interface IndividualPscData {
+    name: String;
+    dob: String;
 }
-export class IndividualPscListHandler extends GenericHandler<PscIndividualListViewData> {
+
+interface IndividualPscListViewData extends BaseViewData {
+    pscIndividuals: IndividualPscData[],
+    companyName: String
+}
+
+export class IndividualPscListHandler extends GenericHandler<IndividualPscListViewData> {
 
     private static templatePath = "router_views/individualPscList/individualPscList";
 
-    public async getViewData (req: Request): Promise<PscIndividualListViewData> {
+    public async getViewData (req: Request): Promise<IndividualPscListViewData> {
 
         const baseViewData = await super.getViewData(req);
         const lang = selectLang(req.query.lang);
@@ -23,9 +26,12 @@ export class IndividualPscListHandler extends GenericHandler<PscIndividualListVi
         const queryParams = new URLSearchParams(req.url.split("?")[1]);
         // hard-coded temporarily
         const companyName = "THE GIRLS' DAY SCHOOL TRUST";
-        // const individualPscList: CompanyPersonWithSignificantControlResource[] = await getCompanyIndividualPscList(req, companyNumber);
-        const pscIndividuals = new Map();
-        pscIndividuals.set("Paul Smith", "2019");
+        // const individualPscList: CompanyPersonWithSignificantControlResource[] = await getCompanyIndividualPscList(req, companyName);
+        const pscIndividuals: IndividualPscData[] = [
+            { name: "Paul Smith", dob: "1999" },
+            { name: "Bertie Bassett", dob: "2008" },
+            { name: "Mellow Yellow", dob: "2004" }
+        ];
 
         return {
             ...baseViewData,
@@ -37,7 +43,7 @@ export class IndividualPscListHandler extends GenericHandler<PscIndividualListVi
         };
     }
 
-    public async executeGet (req: Request, _response: Response): Promise<ViewModel<PscIndividualListViewData>> {
+    public async executeGet (req: Request, _response: Response): Promise<ViewModel<IndividualPscListViewData>> {
         logger.info(`IndividualPscListHandler execute called`);
 
         const viewData = await this.getViewData(req);
