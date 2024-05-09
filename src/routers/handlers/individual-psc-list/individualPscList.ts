a6d84129863385df3ffbeace5c27fef0e79132ce
eedwards0 <eedwards2@companieshouse.gov.uk>
2024-05-09 08:26:13 +0100

    Update view to accomodate psc list WIP

--- src/routers/handlers/individual-psc-list/individualPscList.ts
+++ src/routers/handlers/individual-psc-list/individualPscList.ts
@@ -3,33 +3,43 @@ import { PrefixedUrls } from "../../../constants";
 import { logger } from "../../../lib/logger";
 import { getLocaleInfo, getLocalesService, selectLang } from "../../../utils/localise";
 import { getUrlWithTransactionIdAndSubmissionId } from "../../../utils/url";
-import {
-    BaseViewData,
-    GenericHandler,
-    ViewModel
-} from "../generic";
+import { BaseViewData, GenericHandler, ViewModel } from "../generic";
+// import { CompanyPersonWithSignificantControlResource } from "@companieshouse/api-sdk-node/dist/services/company-psc/types";
+// import { getCompanyIndividualPscList } from "services/companyPscService";
 
-export class IndividualPscListHandler extends GenericHandler<BaseViewData> {
+interface PscIndividualListViewData extends BaseViewData {
+    companyName: String
+    pscIndividuals: Map <String, Date>
+}
+export class IndividualPscListHandler extends GenericHandler<PscIndividualListViewData> {
 
     private static templatePath = "router_views/individualPscList/individualPscList";
 
-    public async getViewData (req: Request): Promise<BaseViewData> {
+    public async getViewData (req: Request): Promise<PscIndividualListViewData> {
 
         const baseViewData = await super.getViewData(req);
         const lang = selectLang(req.query.lang);
         const locales = getLocalesService();
         const queryParams = new URLSearchParams(req.url.split("?")[1]);
+        // hard-coded temporarily
+        const companyName = "THE GIRLS' DAY SCHOOL TRUST";
+        // const individualPscList: CompanyPersonWithSignificantControlResource[] = await getCompanyIndividualPscList(req, companyNumber);
+        const pscIndividuals = new Map();
+        pscIndividuals.set("Paul Smith", "2019");
 
         return {
             ...baseViewData,
             ...getLocaleInfo(locales, lang),
             currentUrl: `${PrefixedUrls.INDIVIDUAL_PSC_LIST}?${queryParams}`,
-            backURL: `${getUrlWithTransactionIdAndSubmissionId(PrefixedUrls.PSC_TYPE, req.params.transactionId, req.params.submissionId)}?${queryParams}`
+            backURL: `${getUrlWithTransactionIdAndSubmissionId(PrefixedUrls.PSC_TYPE, req.params.transactionId, req.params.submissionId)}?${queryParams}`,
+            companyName,
+            pscIndividuals
         };
     }
 
-    public async executeGet (req: Request, _response: Response): Promise<ViewModel<BaseViewData>> {
+    public async executeGet (req: Request, _response: Response): Promise<ViewModel<PscIndividualListViewData>> {
         logger.info(`IndividualPscListHandler execute called`);
+
         const viewData = await this.getViewData(req);
 
         return {
@@ -37,4 +47,5 @@ export class IndividualPscListHandler extends GenericHandler<BaseViewData> {
             viewData
         };
     }
+
 }
