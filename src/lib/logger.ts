86bebde06ca3069e6feff42f5079a0b06501be8b
eedwards0 <eedwards2@companieshouse.gov.uk>
2024-06-06 14:58:38 +0100

    Update to retrieve env.APP_NAME

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,8 +1,9 @@
 import { createLogger } from "@companieshouse/structured-logging-node";
 import ApplicationLogger from "@companieshouse/structured-logging-node/lib/ApplicationLogger";
 import { PSC_VERIFICATION_WEB_NAMESPACE } from "./constants";
+import { env } from "../config";
 
-export const logger: ApplicationLogger = createLogger(process.env.APP_NAME ?? PSC_VERIFICATION_WEB_NAMESPACE);
+export const logger: ApplicationLogger = createLogger(env.APP_NAME ?? PSC_VERIFICATION_WEB_NAMESPACE);
 
 // tslint:disable-next-line:no-console
 console.log(`env.LOG_LEVEL set to ${process.env.LOG_LEVEL}`);
