12dd5063e28cef2a2eb4512d27ba4482933b065e
JasSmith-ch <jsmith3@companieshouse.gov.uk>
2024-06-12 10:18:04 +0100

    refactored psc type handler and router

--- src/routers/pscTypeRouter.ts
+++ src/routers/pscTypeRouter.ts
@@ -3,17 +3,14 @@ import { PrefixedUrls } from "../constants";
 import { handleExceptions } from "../utils/asyncHandler";
 import { selectLang } from "../utils/localise";
 import { getUrlWithTransactionIdAndSubmissionId } from "../utils/url";
-import { PscTypeHandler } from "./handlers/psc-type/pscType";
+import { PscTypeHandler } from "./handlers/psc-type/pscTypeHandler";
 
 const pscTypeRouter: Router = Router({ mergeParams: true });
 
 pscTypeRouter.get("/", handleExceptions(async (req: Request, res: Response, _next: NextFunction) => {
     const handler = new PscTypeHandler();
-    const params = await handler.executeGet(req, res);
-
-    if (params.templatePath && params.viewData) {
-        res.render(params.templatePath, params.viewData);
-    }
+    const { templatePath, viewData } = await handler.executeGet(req, res);
+    res.render(templatePath, viewData);
 }));
 
 pscTypeRouter.post("/", handleExceptions(async (req: Request, res: Response, _next: NextFunction) => {
