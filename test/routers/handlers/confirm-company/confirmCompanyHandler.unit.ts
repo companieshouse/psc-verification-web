a4c4c5ecd0431d128140ff9b1979eb5f5466d5ce
Alison Griffiths <agriffiths1@companieshouse.gov.uk>
2024-06-14 11:05:31 +0100

    Initial commit for the refactor of confirm company tests

---
+++ test/routers/handlers/confirm-company/confirmCompanyHandler.unit.ts
@@ -0,0 +1,8 @@
+describe("confirm company handler tests", () => {
+    describe("executeGet tests", () => {
+        it("Should render the confirmCompany screen", async () => {});
+    });
+    describe("executePost tests", () => {
+        it("Should redirect to the psc-type screen", async () => {});
+    });
+});
