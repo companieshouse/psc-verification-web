426c18656302cf30c2a23869ee5eb542b2eaaa9f
eedwards0 <eedwards2@companieshouse.gov.uk>
2024-07-09 15:53:57 +0100

    wip

--- test/routers/handlers/personal-code/personalCode.int.ts
+++ test/routers/handlers/personal-code/personalCode.int.ts
@@ -6,7 +6,7 @@ import mockAuthenticationMiddleware from "../../../mocks/authenticationMiddlewar
 import { PrefixedUrls } from "../../../../src/constants";
 import { getUrlWithTransactionIdAndSubmissionId } from "../../../../src/utils/url";
 import { PSC_INDIVIDUAL } from "../../../mocks/psc.mock";
-import { INDIVIDUAL_VERIFICATION_FULL, PATCH_INDIVIDUAL_DATA, PSC_VERIFICATION_ID, TRANSACTION_ID } from "../../../mocks/pscVerification.mock";
+import { INDIVIDUAL_VERIFICATION_PATCH, PATCH_INDIVIDUAL_DATA, PSC_VERIFICATION_ID, TRANSACTION_ID } from "../../../mocks/pscVerification.mock";
 import { getPscVerification, patchPscVerification } from "../../../../src/services/pscVerificationService";
 import app from "../../../../src/app";
 import { PscVerificationData } from "@companieshouse/api-sdk-node/dist/services/psc-verification-link/types";
@@ -14,10 +14,6 @@ import { IncomingMessage } from "http";
 
 jest.mock("../../../../src/services/pscVerificationService");
 const mockGetPscVerification = getPscVerification as jest.Mock;
-mockGetPscVerification.mockResolvedValue({
-    httpStatusCode: HttpStatusCode.Ok,
-    resource: INDIVIDUAL_VERIFICATION_FULL
-});
 const mockPatchPscVerification = patchPscVerification as jest.Mock;
 
 jest.mock("../../../../src/services/pscService", () => ({
@@ -27,55 +23,65 @@ jest.mock("../../../../src/services/pscService", () => ({
     })
 }));
 
-describe("personal code view", () => {
+describe("personal code router/handler integration tests", () => {
 
     beforeEach(() => {
-        mockSessionMiddleware.mockClear();
-        mockAuthenticationMiddleware.mockClear();
+        jest.clearAllMocks();
+        mockGetPscVerification.mockResolvedValue({
+            httpStatusCode: HttpStatusCode.Ok,
+            resource: INDIVIDUAL_VERIFICATION_PATCH
+        });
     });
 
     afterEach(() => {
         expect(mockSessionMiddleware).toHaveBeenCalledTimes(1);
         expect(mockAuthenticationMiddleware).toHaveBeenCalledTimes(1);
-        jest.clearAllMocks();
+        expect(mockGetPscVerification).toHaveBeenCalled();
     });
 
-    it("Should render the Personal code page with a success status code and correct links", async () => {
+    describe("GET method", () => {
 
-        const queryParams = new URLSearchParams("lang=en");
-        const uriWithQuery = `${PrefixedUrls.PERSONAL_CODE}?${queryParams}`;
-        const uri = getUrlWithTransactionIdAndSubmissionId(uriWithQuery, TRANSACTION_ID, PSC_VERIFICATION_ID);
+        it("Should render the Personal code page with a success status code and correct links", async () => {
 
-        const resp = await request(app).get(uri);
+            const queryParams = new URLSearchParams("lang=en");
+            const uriWithQuery = `${PrefixedUrls.PERSONAL_CODE}?${queryParams}`;
+            const uri = getUrlWithTransactionIdAndSubmissionId(uriWithQuery, TRANSACTION_ID, PSC_VERIFICATION_ID);
 
-        const $ = cheerio.load(resp.text);
+            const resp = await request(app).get(uri)
+                .expect(HttpStatusCode.Ok);
 
-        expect(resp.status).toBe(HttpStatusCode.Ok);
+            const $ = cheerio.load(resp.text);
 
-        expect($("a.govuk-back-link").attr("href")).toBe("/persons-with-significant-control-verification/transaction/11111-22222-33333/submission/662a0de6a2c6f9aead0f32ab/individual/psc-list?lang=en");
-    });
-
-    it("Should redirect to the PSC individual statement page with a redirect status code", async () => {
-        const uri = getUrlWithTransactionIdAndSubmissionId(PrefixedUrls.PERSONAL_CODE, TRANSACTION_ID, PSC_VERIFICATION_ID);
-        const verification: PscVerificationData = {
-            verificationDetails: {
-                uvid: "123abc456edf"
-            }
-        };
-        mockPatchPscVerification.mockResolvedValueOnce({
-            HttpStatusCode: HttpStatusCode.Ok,
-            resource: {
-                ...PATCH_INDIVIDUAL_DATA, ...verification
-            }
+            expect($("a.govuk-back-link").attr("href")).toBe("/persons-with-significant-control-verification/transaction/11111-22222-33333/submission/662a0de6a2c6f9aead0f32ab/individual/psc-list?lang=en");
         });
-
-        const resp = await request(app)
-            .post(uri)
-            .send({ personalCode: "123abc456edf" });
-
-        expect(resp.status).toBe(HttpStatusCode.Found);
-        expect(mockPatchPscVerification).toHaveBeenCalledTimes(1);
-        expect(mockPatchPscVerification).toHaveBeenCalledWith(expect.any(IncomingMessage), TRANSACTION_ID, PSC_VERIFICATION_ID, verification);
-        expect(resp.header.location).toBe(`${getUrlWithTransactionIdAndSubmissionId(PrefixedUrls.INDIVIDUAL_STATEMENT, TRANSACTION_ID, PSC_VERIFICATION_ID)}?lang=en`);
     });
+
+    describe("POST method", () => {
+
+        it("Should redirect to the PSC individual statement page with a redirect status code", async () => {
+            const uri = getUrlWithTransactionIdAndSubmissionId(PrefixedUrls.PERSONAL_CODE, TRANSACTION_ID, PSC_VERIFICATION_ID);
+            const verification: PscVerificationData = {
+                verificationDetails: {
+                    uvid: "123abc456edf"
+                }
+            };
+            mockPatchPscVerification.mockResolvedValueOnce({
+                HttpStatusCode: HttpStatusCode.Ok,
+                resource: {
+                    ...PATCH_INDIVIDUAL_DATA, ...verification
+                }
+            });
+
+            const resp = await request(app)
+                .post(uri)
+                .send({ personalCode: "123abc456edf" });
+
+            expect(resp.status).toBe(HttpStatusCode.Found);
+            expect(mockPatchPscVerification).toHaveBeenCalledTimes(1);
+            expect(mockGetPscVerification).toHaveBeenCalledTimes(1);
+            expect(mockPatchPscVerification).toHaveBeenCalledWith(expect.any(IncomingMessage), TRANSACTION_ID, PSC_VERIFICATION_ID, verification);
+            expect(resp.header.location).toBe(`${getUrlWithTransactionIdAndSubmissionId(PrefixedUrls.INDIVIDUAL_STATEMENT, TRANSACTION_ID, PSC_VERIFICATION_ID)}?lang=en`);
+        });
+    });
+
 });
