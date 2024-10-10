e76a822eeda9d10b02af322ae1c573e34b183f93
Moses Wejuli <mwejuli@companieshouse.gov.uk>
2024-10-10 15:18:19 +0100

    fix: ensure csrf token is readable by the app on form submissions      - cleans up folder paths for views in node_modules      - modifies comments in app.ts to be more descriptive

--- src/app.ts
+++ src/app.ts
@@ -9,19 +9,16 @@ import routerDispatch from "./routerDispatch";
 import { isLive } from "./middleware/serviceLive";
 import { csrfProtectionMiddleware } from "./middleware/csrf";
 
-
 const app = express();
+
+// service availability page
 app.use(isLive);
 
 app.set("views", [
     path.join(__dirname, "views"),
-    path.join(__dirname, "node_modules/govuk-frontend"),
-    path.join(__dirname, "../node_modules/govuk-frontend"), // This if for when using ts-node since the working directory is src
-    path.join(__dirname, "node_modules/govuk-frontend/components"),
-    path.join(__dirname, "../node_modules/@companieshouse/ch-node-utils/templates/"),
-    path.join(__dirname, "node_modules/@companieshouse/ch-node-utils/templates/"),
-    path.join(__dirname, "../node_modules/@companieshouse/web-security-node/components"),
-    path.join(__dirname, "node_modules/@companieshouse/web-security-node/components")
+    path.join(__dirname, "./../node_modules/govuk-frontend"), // This if for when using ts-node since the working directory is src
+    path.join(__dirname, "./../node_modules/@companieshouse/ch-node-utils/templates/"),
+    path.join(__dirname, "./../node_modules/@companieshouse/web-security-node/components")
 ]);
 
 const nunjucksLoaderOpts = {
@@ -37,13 +34,21 @@ const njk = new nunjucks.Environment(
 njk.express(app);
 app.set("view engine", "njk");
 
-// apply middleware
+// get cookie data from incoming request
 app.use(cookieParser());
+
+// parse incoming payload into req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// initiate session and attach to middleware
 app.use(servicePathPrefix, sessionMiddleware);
+
+// attach csrf protection to middleware
 app.use(csrfProtectionMiddleware);
 
-// Serve static files
-app.use(express.static(path.join(__dirname, "/../assets/public")));
+// serve static files
+app.use(express.static(path.join(__dirname, "./../assets/public")));
 
 njk.addGlobal("cdnUrlCss", process.env.CDN_URL_CSS);
 njk.addGlobal("cdnUrlJs", process.env.CDN_URL_JS);
@@ -56,29 +61,25 @@ njk.addGlobal("PIWIK_SITE_ID", process.env.PIWIK_SITE_ID);
 njk.addGlobal("PIWIK_URL", process.env.PIWIK_URL);
 njk.addGlobal("verifyIdentityLink", process.env.VERIFY_IDENTITY_LINK);
 
-// If app is behind a front-facing proxy, and to use the X-Forwarded-* headers to determine the connection and the IP address of the client
+// if app is behind a front-facing proxy, and to use the X-Forwarded-* headers to determine the connection and the IP address of the client
 app.enable("trust proxy");
 
-// parse body into req.body
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-// Unhandled errors
+// unhandled errors
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
     logger.error(`${err.name} - appError: ${err.message} - ${err.stack}`);
     res.render("partials/error_500");
 });
 
-// Channel all requests through router dispatch
+// channel all requests through router dispatch
 routerDispatch(app);
 
-// Unhandled exceptions
+// unhandled exceptions
 process.on("uncaughtException", (err: any) => {
     logger.error(`${err.name} - uncaughtException: ${err.message} - ${err.stack}`);
     process.exit(1);
 });
 
-// Unhandled promise rejections
+// unhandled promise rejections
 process.on("unhandledRejection", (err: any) => {
     logger.error(`${err.name} - unhandledRejection: ${err.message} - ${err.stack}`);
     process.exit(1);
