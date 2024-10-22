b1bf7172a9586aa025d6dd9964f7e3700b8db602
Simon Boyd <sboyd@companieshouse.gov.uk>
2024-10-22 09:33:51 +0100

    Fix app paths for ECS

--- src/app.ts
+++ src/app.ts
@@ -17,9 +17,12 @@ app.use(isLive);
 
 app.set("views", [
     path.join(__dirname, "views"),
-    path.join(__dirname, "./../node_modules/govuk-frontend"), // This if for when using ts-node since the working directory is src
-    path.join(__dirname, "./../node_modules/@companieshouse/ch-node-utils/templates/"),
-    path.join(__dirname, "./../node_modules/@companieshouse/web-security-node/components")
+    path.join(__dirname, "../node_modules/govuk-frontend"), // This if for when using ts-node since the working directory is src
+    path.join(__dirname, "node_modules/govuk-frontend"),
+    path.join(__dirname, "../node_modules/@companieshouse/ch-node-utils/templates/"),
+    path.join(__dirname, "node_modules/@companieshouse/ch-node-utils/templates/"),
+    path.join(__dirname, "../node_modules/@companieshouse/web-security-node/components"),
+    path.join(__dirname, "node_modules/@companieshouse/web-security-node/components")
 ]);
 
 const nunjucksLoaderOpts = {
