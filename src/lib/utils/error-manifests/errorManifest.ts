f11b4edd923c4d08bfa98e2c2892f28df5b63a91
eedwards0 <eedwards2@companieshouse.gov.uk>
2024-11-26 10:39:59 +0000

    Tidied up errorManifest

--- src/lib/utils/error-manifests/errorManifest.ts
+++ src/lib/utils/error-manifests/errorManifest.ts
@@ -5,56 +5,19 @@ const localesService = getLocalesService();
 const errorManifest = (lang: string = "en", attribute: string = "") => ({
     generic: {
         serverError: {
-            summary: localesService.i18nCh.resolveSingleKey("server_error_summary", lang) || "There was an error processing your request. Please try again."
+            summary: localesService.i18nCh.resolveSingleKey("server_error_summary", lang)
         }
     },
 
     validation: {
         default: {
-            summary: localesService.i18nCh.resolveSingleKey("validation_error_summary", lang) || "Your request contains validation errors",
-            inline: localesService.i18nCh.resolveSingleKey("validation_error_inline", lang) || "Your request contains validation errors"
-        },
-        firstName: {
-            blank: {
-                summary: "Enter the first name",
-                inline: "Enter the first name"
-            },
-            incorrect: {
-                summary: "Enter a valid firstname",
-                inline: "Enter a valid firstname"
-            }
-        },
-        middleName: {
-            blank: {},
-            incorrect: {
-                summary: "Enter a valid middle name",
-                inline: "Enter a valid middle name"
-            }
-        },
-        lastName: {
-            blank: {
-                summary: "Enter the last name",
-                inline: "Enter the last name"
-            },
-            incorrect: {
-                summary: "Enter a valid last name",
-                inline: "Enter a valid last name"
-            }
-        },
-        dateOfBirth: {
-            blank: {
-                summary: "Enter the date of birth",
-                inline: "Enter the date of birth"
-            },
-            incorrect: {
-                summary: "Enter a valid date of birth",
-                inline: "Enter a valid  date of birth"
-            }
+            summary: localesService.i18nCh.resolveSingleKey("validation_error_summary", lang),
+            inline: localesService.i18nCh.resolveSingleKey("validation_error_inline", lang)
         },
         pscType: {
             blank: {
-                summary: localesService.i18nCh.resolveSingleKey("psc_type_error_summary", lang) || "Select if you're providing verification details for a PSC or RLE",
-                inline: localesService.i18nCh.resolveSingleKey("psc_type_error_inline", lang) || "Inline: Select if you're providing verification details for a PSC or RLE"
+                summary: localesService.i18nCh.resolveSingleKey("psc_type_error_summary", lang),
+                inline: localesService.i18nCh.resolveSingleKey("psc_type_error_inline", lang)
             },
             incorrect: {}
         },
@@ -74,8 +37,8 @@ const errorManifest = (lang: string = "en", attribute: string = "") => ({
         },
         personalCode: {
             blank: {
-                summary: (localesService.i18nCh.resolveSingleKey("personal_code_error_summary", lang) + attribute || "Enter the Companies House personal code for " + attribute),
-                inline: (localesService.i18nCh.resolveSingleKey("personal_code_error_inline", lang) + attribute || "Inline: Enter the Companies House personal code for " + attribute)
+                summary: (localesService.i18nCh.resolveSingleKey("personal_code_error_summary", lang) + attribute),
+                inline: (localesService.i18nCh.resolveSingleKey("personal_code_error_inline", lang) + attribute)
             },
             incorrect: {}
         },
@@ -83,13 +46,6 @@ const errorManifest = (lang: string = "en", attribute: string = "") => ({
             blank: {
                 summary: "Confirm if the identity verification statement is correct",
                 inline: "Confirm if the identity verification statement is correct"
-            },
-            incorrect: {}
-        },
-        relevantOfficerStatementConfirmation: {
-            blank: {
-                summary: "Summary error text for relevantOfficerStatementConfirmation to be provided",
-                inline: "Inline error text for relevantOfficerStatementConfirmation to be provided"
             },
             incorrect: {}
         }
