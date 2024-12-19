edccf2a35d2417ae49315faaeadf2184e8b1786d
PhilipGlover <pglover@companieshouse.gov.uk>
2024-12-19 11:57:44 +0000

    add checkCompany middleware to check for allowed company type and status
    - also add api-enumerations git submodule for lists of allowed values
    - removes 2 blank unneeded files

---
+++ src/middleware/checkCompany.ts
@@ -0,0 +1,26 @@
+import { CompanyProfile } from "@companieshouse/api-sdk-node/dist/services/company-profile/types";
+import { NextFunction, Request, Response } from "express";
+import { COMPANY_STATUS_NOT_ALLOWED, COMPANY_TYPE_ALLOWED, getPscVerificationCompanyLists } from "../config/api.enumerations";
+import { PrefixedUrls, STOP_TYPE } from "../constants";
+import { logger } from "../lib/logger";
+import { getUrlWithStopType } from "../utils/url";
+import { addSearchParams } from "../utils/queryParams";
+
+export const checkCompany = (req: Request, res: Response, next: NextFunction) => {
+    const companyProfile: CompanyProfile = res.locals?.companyProfile;
+    const companyNumber = companyProfile.companyNumber;
+    const lang = req.query.lang as string;
+
+    const companyStatusNotAllowed: string[] = getPscVerificationCompanyLists(COMPANY_STATUS_NOT_ALLOWED);
+    const companyTypeAllowed: string[] = getPscVerificationCompanyLists(COMPANY_TYPE_ALLOWED);
+
+    if (companyStatusNotAllowed.includes(companyProfile.companyStatus)) {
+        logger.debug(`${checkCompany.name} - Company Status ${companyProfile.companyStatus} for company number ${companyNumber} is not allowed`);
+        res.redirect(addSearchParams(getUrlWithStopType(PrefixedUrls.STOP_SCREEN, STOP_TYPE.COMPANY_STATUS), { companyNumber, lang }));
+    } else if (!companyTypeAllowed.includes(companyProfile.type)) {
+        logger.debug(`${checkCompany.name} - Company Type ${companyProfile.type} for company number ${companyNumber} is not allowed`);
+        res.redirect(addSearchParams(getUrlWithStopType(PrefixedUrls.STOP_SCREEN, STOP_TYPE.COMPANY_TYPE), { companyNumber, lang }));
+    }
+
+    next();
+};
