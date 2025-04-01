31cf1bd8dcd970ed07266120905e6c46c24fe573
eedwards0 <eedwards2@companieshouse.gov.uk>
2025-04-01 15:37:41 +0100

    Remove temp code and revert dependency to main sdk branch

--- src/routers/handlers/individual-psc-list/individualPscListHandler.ts
+++ src/routers/handlers/individual-psc-list/individualPscListHandler.ts
@@ -8,7 +8,6 @@ import { BaseViewData, GenericHandler, ViewModel } from "../generic";
 import { formatDateBorn, internationaliseDate } from "../../utils";
 import { env } from "../../../config";
 import { logger } from "../../../lib/logger";
-import { getPscIndividual } from "../../../services/pscService";
 
 interface PscListData {
     pscId: string,
@@ -65,24 +64,6 @@ export class IndividualPscListHandler extends GenericHandler<IndividualPscListVi
 
         const exclusivelySuperSecure = allPscDetails.length > 0 && (allSuperSecure && !allCeased);
         const showNoPscsMessage = allPscDetails.length === 0 || allCeased;
-
-        // TODO - use/display the verification state data in the PSC list page
-        // Temporary block to retrieve a single PSC verification state when running in Docker and CHIPS cidev
-        if (allPscDetails && allPscDetails.length > 0) {
-            const notificationId = "PSCDATA5";
-            const pscId = "PSCDATA5";
-            const pscData = allPscDetails.find(item => item.pscId === pscId);
-            let pscWithVerificationState;
-
-            if (pscData) {
-                try {
-                    pscWithVerificationState = await getPscIndividual(companyNumber, notificationId);
-                    logger.debug(`PSC with verification state: ${JSON.stringify(pscWithVerificationState)}`);
-                } catch (err: any) {
-                    logger.error(`${req.method} error: problem calling getPscVerificationState request: ${err.message}`);
-                }
-            }
-        }
 
         return {
             ...baseViewData,
