b8ea60372091b1ee0314a3709d8fcb7ed131e1dd
kieo-ch <koleary1@companieshouse.gov.uk>
2025-06-26 09:10:09 +0100

    Update all packages flagged by ncu

--- src/app.ts
+++ src/app.ts
@@ -24,8 +24,8 @@ app.use(isLive);
 
 app.set("views", [
     path.join(__dirname, "views"),
-    path.join(__dirname, "../node_modules/govuk-frontend"), // This if for when using ts-node since the working directory is src
-    path.join(__dirname, "node_modules/govuk-frontend"),
+    path.join(__dirname, "../node_modules/govuk-frontend/dist"), // This if for when using ts-node since the working directory is src
+    path.join(__dirname, "node_modules/govuk-frontend/dist"),
     path.join(__dirname, "../node_modules/@companieshouse/ch-node-utils/templates/"),
     path.join(__dirname, "node_modules/@companieshouse/ch-node-utils/templates/"),
     path.join(__dirname, "../node_modules/@companieshouse/web-security-node/components"),
