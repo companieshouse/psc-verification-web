7b9b1f56f204701954086f91fd0509fc5d4f9e7e
kieo-ch <koleary1@companieshouse.gov.uk>
2025-07-24 11:05:11 +0100

    Add viewdata to csrfErrorHandler

--- src/middleware/error-interceptors/csrfErrorInterceptor.ts
+++ src/middleware/error-interceptors/csrfErrorInterceptor.ts
@@ -1,21 +1,27 @@
 import { NextFunction, Request, Response } from "express";
 import { CsrfError } from "@companieshouse/web-security-node";
 import { HttpStatusCode } from "axios";
+import { getViewData } from "../../routers/handlers/generic";
+import { getLocaleInfo, getLocalesService, selectLang } from "../../utils/localise";
 
 const csrfErrorTemplateName = "error/403-forbidden";
 
-const csrfErrorHandler = (
-    err: CsrfError | Error,
-    _: Request,
-    res: Response,
-    next: NextFunction
-) => {
+const csrfErrorHandler = (err: CsrfError | Error, req: Request, res: Response, next: NextFunction) => {
     if (!(err instanceof CsrfError)) {
         return next(err);
     }
 
-    return res.status(HttpStatusCode.Forbidden).render(csrfErrorTemplateName, {
-        csrfErrors: true
+    const lang = selectLang(req.query.lang);
+    const locales = getLocalesService();
+
+    getViewData(req, res, lang).then((baseViewData) => {
+        return res.status(HttpStatusCode.Forbidden).render(csrfErrorTemplateName, {
+            ...baseViewData,
+            ...getLocaleInfo(locales, lang),
+            currentUrl: req.originalUrl,
+            templateName: csrfErrorTemplateName,
+            csrfErrors: true
+        });
     });
 };
 
