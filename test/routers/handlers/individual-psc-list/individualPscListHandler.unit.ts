178a4c0dc0c5df2936ccd280194bd7619d561ec5
kieo-ch <koleary1@companieshouse.gov.uk>
2025-08-18 09:38:49 +0100

    Add unit tests for verification predicates

--- test/routers/handlers/individual-psc-list/individualPscListHandler.unit.ts
+++ test/routers/handlers/individual-psc-list/individualPscListHandler.unit.ts
@@ -1,7 +1,7 @@
 import { HttpStatusCode } from "axios";
 import * as httpMocks from "node-mocks-http";
 import { PSC_KIND_TYPE, Urls } from "../../../../src/constants";
-import { IndividualPscListHandler } from "../../../../src/routers/handlers/individual-psc-list/individualPscListHandler";
+import { IndividualPscListHandler, pscCanVerifyLater, pscCanVerifyNow, pscIsUnverified, pscIsVerified } from "../../../../src/routers/handlers/individual-psc-list/individualPscListHandler";
 import { getCompanyProfile } from "../../../../src/services/companyProfileService";
 import { getCompanyIndividualPscList } from "../../../../src/services/companyPscService";
 import { getPscIndividual } from "../../../../src/services/pscService";
@@ -9,6 +9,7 @@ import { getPscVerification } from "../../../../src/services/pscVerificationServ
 import { validCompanyProfile } from "../../../mocks/companyProfile.mock";
 import { INDIVIDUAL_PSCS_LIST, SUPER_SECURE_PSCS_EXCLUSIVE_LIST, VERIFIED_PSC, VERIFY_LATER_PSC, VERIFY_NOW_PSC } from "../../../mocks/companyPsc.mock";
 import { COMPANY_NUMBER, INDIVIDUAL_VERIFICATION_CREATED } from "../../../mocks/pscVerification.mock";
+import { PersonWithSignificantControl } from "@companieshouse/api-sdk-node/dist/services/psc/types";
 
 const mockGetPscVerification = getPscVerification as jest.Mock;
 jest.mock("../../../../src/services/pscVerificationService");
@@ -66,6 +67,79 @@ describe("psc list handler", () => {
             viewData.canVerifyNowDetails.forEach(p => expect(p.pscKind).toBe(PSC_KIND_TYPE.INDIVIDUAL));
             viewData.canVerifyLaterDetails.forEach(p => expect(p.pscKind).toBe(PSC_KIND_TYPE.INDIVIDUAL));
             viewData.verifiedPscDetails.forEach(p => expect(p.pscKind).toBe(PSC_KIND_TYPE.INDIVIDUAL));
+        });
+    });
+
+    describe("IndividualPscListHandler Predicates", () => {
+
+        const mockPsc = (overrides: Partial<PersonWithSignificantControl>): PersonWithSignificantControl => ({
+            identityVerificationDetails: {},
+            ...overrides
+        } as PersonWithSignificantControl);
+
+        describe("pscIsVerified", () => {
+            it("should return true if verification start date is in the past and end date is in the future", () => {
+                const psc = mockPsc({
+                    identityVerificationDetails: {
+                        appointmentVerificationStartOn: new Date("2025-08-01"),
+                        appointmentVerificationEndOn: new Date("2025-08-31")
+                    }
+                });
+                expect(pscIsVerified(psc)).toBe(true);
+                expect(pscIsUnverified(psc)).toBe(false);
+            });
+
+            it("should return false if verification start date is in the future", () => {
+                const psc = mockPsc({
+                    identityVerificationDetails: {
+                        appointmentVerificationStartOn: new Date("2025-09-01"),
+                        appointmentVerificationEndOn: new Date("2025-09-30")
+                    }
+                });
+                expect(pscIsVerified(psc)).toBe(false);
+                expect(pscIsUnverified(psc)).toBe(true);
+            });
+
+            it("should return false if verification end date is in the past", () => {
+                const psc = mockPsc({
+                    identityVerificationDetails: {
+                        appointmentVerificationStartOn: new Date("2025-07-01"),
+                        appointmentVerificationEndOn: new Date("2025-07-31")
+                    }
+                });
+                expect(pscIsVerified(psc)).toBe(false);
+                expect(pscIsUnverified(psc)).toBe(true);
+            });
+        });
+
+        describe("pscCanVerifyNow", () => {
+            it("should return true if verification statement date is undefined", () => {
+                const psc = mockPsc({
+                    identityVerificationDetails: {}
+                });
+                expect(pscCanVerifyNow(psc)).toBe(true);
+                expect(pscCanVerifyLater(psc)).toBe(false);
+            });
+
+            it("should return true if verification statement date is in the past", () => {
+                const psc = mockPsc({
+                    identityVerificationDetails: {
+                        appointmentVerificationStatementDate: new Date("2025-08-01")
+                    }
+                });
+                expect(pscCanVerifyNow(psc)).toBe(true);
+                expect(pscCanVerifyLater(psc)).toBe(false);
+            });
+
+            it("should return false if verification statement date is in the future", () => {
+                const psc = mockPsc({
+                    identityVerificationDetails: {
+                        appointmentVerificationStatementDate: new Date("2025-09-01")
+                    }
+                });
+                expect(pscCanVerifyNow(psc)).toBe(false);
+                expect(pscCanVerifyLater(psc)).toBe(true);
+            });
         });
     });
 });
