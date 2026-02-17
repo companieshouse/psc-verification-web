8c0d79b5afdf86ea8d613b6b4523b28faf47da3f
AlisonGriffiths <agriffiths1@companieshouse.gov.uk>
2026-02-17 09:41:39 +0000

    Removed unnecessary assertions from openTelemetryConfig sonar issue

--- src/open-telemetry/openTelemetryConfig.ts
+++ src/open-telemetry/openTelemetryConfig.ts
@@ -4,18 +4,18 @@ import { env } from "../config";
 const otlpEndpoint = env.OTEL_EXPORTER_OTLP_ENDPOINT;
 
 const openTelemetryConfig: ApplicationConfiguration = {
-    port: parseInt(env.PSC_VERIFICATION_WEB_PORT as string),
-    apiAddress: env.INTERNAL_API_URL as string,
-    internalApiKey: env.CHS_INTERNAL_API_KEY as string,
+    port: parseInt(env.PSC_VERIFICATION_WEB_PORT),
+    apiAddress: env.INTERNAL_API_URL,
+    internalApiKey: env.CHS_INTERNAL_API_KEY,
     env: (env.NODE_ENV || "development").toLowerCase(),
     urlPrefix: "persons-with-significant-control-verification",
     session: {
-        cookieName: env.COOKIE_NAME as string,
-        cookieSecret: env.COOKIE_SECRET as string,
-        cookieDomain: env.COOKIE_DOMAIN as string,
-        cacheServer: env.CACHE_SERVER as string
+        cookieName: env.COOKIE_NAME,
+        cookieSecret: env.COOKIE_SECRET,
+        cookieDomain: env.COOKIE_DOMAIN,
+        cacheServer: env.CACHE_SERVER
     },
-    applicationNamespace: env.APP_NAME as string,
+    applicationNamespace: env.APP_NAME,
     baseUrl: env.CHS_URL ?? "",
     otel: {
         traceExporterUrl: `${otlpEndpoint}/v1/traces`,
