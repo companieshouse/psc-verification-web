2e8c5342e8c5ffed3c7a1f67221efcb50ada7a6c
AlisonGriffiths <agriffiths1@companieshouse.gov.uk>
2026-02-26 08:27:03 +0000

    ASM-1541 fixing open telemetry startup error

--- ecs-image-build/docker_start.sh
+++ ecs-image-build/docker_start.sh
@@ -3,4 +3,4 @@
 # Start script for PSC verification web
 PORT=3000
 export NODE_PORT=${PORT}
-exec node -r /opt/otel.js httpserver.js -- ${PORT}
+exec node -r ./dist/otel.js httpserver.js -- ${PORT}
